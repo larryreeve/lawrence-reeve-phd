9903417820098e3c0b990bcb37e27c0268a1ad7a
larryreeve <larry.reeve@gmail.com>
2023-08-15 17:21:56 -0400

    Initial commit

--- data/Source/TFDSSummarizer/src/client/service/ISummarizerServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package client.service;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface ISummarizerServiceAsync
-{
-    public void summarizeTFDS(String textToSummarize, Integer sourcePctOfSentences, AsyncCallback callback);
-}
